810247f3e73be81897a2bea3fc90f5c18244ba08
Blake Moss <Blake Moss>
2017-03-27 15:17:52 -0600

    changes

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ app.put('/', function(req, res){
     return res.status('401').send("Not logged in! Go to /signin!");
   }
   else {
-    var key = req.body.qs.key;
+    var key = req;
     console.log("In PUT function: " + key);
     keyob = new Object();
     keyob.key = key;
