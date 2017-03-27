bfb52b6ac033fb1944b7d200d6584191572a0e01
Blake Moss <Blake Moss>
2017-03-27 14:52:26 -0600

    changes

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ app.put('/', function(req, res){
     console.log("In PUT function: " + key);
     keyob = new Object();
     keyob.key = key;
-    keyob.value = value;
+  
     req.user.keypair.push(keyob);
     console.log(req.user);
       return res.status('200').send("Key Value Pairs for " + req.user.username + " : " + req.user.keypair);
